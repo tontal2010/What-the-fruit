d3babcba3498de3f71dc42b0a996cdc2ac45c3a5
zGalactose <tontal27@gmail.com>
2020-03-17 18:09:50 +0700

    เริ่มต้นนะ

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ global not_END
 global input_fact
 
 not_END = True
+input_fact = True
 KB = [["orange", "circle", "orange"], ["yellow", "long", "banana"]]
 print('Enter fact:')
+
 
 input_fact = input()
 
