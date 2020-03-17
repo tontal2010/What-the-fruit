672a3f24a9a5f48cbb1742e18dccca15c6765b4c
zGalactose <tontal27@gmail.com>
2020-03-17 18:08:42 +0700

    เริ่มต้นนะ

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+global KB
+global not_END
+global input_fact
+
+not_END = True
+KB = [["orange", "circle", "orange"], ["yellow", "long", "banana"]]
+print('Enter fact:')
+
+input_fact = input()
 
 
-# fact
+def add_fact_to_BB():
+
+while not_END:
